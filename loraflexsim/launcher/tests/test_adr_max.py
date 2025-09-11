41cf9d074019000f39c4ff762ab6c3fb95b3ecbd
rot226 <160727202+rot226@users.noreply.github.com>
2025-09-11 09:30:32 +0000

    test: expand adr-max coverage

--- loraflexsim/launcher/tests/test_adr_max.py
+++ loraflexsim/launcher/tests/test_adr_max.py
@@ -23,15 +23,16 @@ def test_adr_max_apply_configures_simulator():
     assert sim.adr_server is True
     assert sim.network_server.adr_enabled is True
     assert sim.network_server.adr_method == "adr-max"
-    assert node.sf == 12
-    expected_thr = Channel.flora_detection_threshold(12, node.channel.bandwidth) + node.channel.sensitivity_margin_dB
+    expected_thr = Channel.flora_detection_threshold(
+        node.sf, node.channel.bandwidth
+    ) + node.channel.sensitivity_margin_dB
     assert node.channel.detection_threshold_dBm == expected_thr
     assert node.tx_power == 14.0
     assert node.adr_ack_limit == 64
     assert node.adr_ack_delay == 32
 
 
-def test_adr_max_server_uses_snr_history():
+def test_adr_max_progressively_reduces_rate():
     sim = Simulator(
         num_nodes=1,
         num_gateways=1,
@@ -47,9 +48,50 @@ def test_adr_max_server_uses_snr_history():
     node = sim.nodes[0]
     ns = sim.network_server
     noise = ns.channel.noise_floor_dBm()
-    rssi = noise + server.REQUIRED_SNR[12] + server.MARGIN_DB + 30.0
-    for i in range(20):
-        ns.receive(i + 1, node.id, sim.gateways[0].id, rssi)
-    assert node.sf == 7
-    assert node.tx_power == 4.0
+
+    initial_sf = node.sf
+    initial_power = node.tx_power
+
+    for cycle in range(3):
+        rssi = noise + server.REQUIRED_SNR[node.sf] + server.MARGIN_DB + 6.0
+        for i in range(20):
+            ns.receive(cycle * 20 + i + 1, node.id, sim.gateways[0].id, rssi)
+        if cycle == 0:
+            assert node.sf == initial_sf - 2
+            assert node.tx_power == initial_power
+        elif cycle == 1:
+            assert node.sf == initial_sf - 4
+            assert node.tx_power == initial_power
+        else:
+            assert node.sf == 7
+            assert node.tx_power == initial_power - 2.0
+        assert node.snr_history == []
+
+
+def test_adr_max_margin_uses_max_snr_history():
+    sim = Simulator(
+        num_nodes=1,
+        num_gateways=1,
+        transmission_mode="Periodic",
+        packet_interval=1.0,
+        duty_cycle=None,
+        mobility=False,
+        adr_node=False,
+        adr_server=False,
+        seed=1,
+    )
+    adr_max.apply(sim)
+    node = sim.nodes[0]
+    ns = sim.network_server
+    noise = ns.channel.noise_floor_dBm()
+
+    low = noise + server.REQUIRED_SNR[node.sf] + server.MARGIN_DB - 2.0
+    high = noise + server.REQUIRED_SNR[node.sf] + server.MARGIN_DB + 6.0
+
+    ns.receive(1, node.id, sim.gateways[0].id, high)
+    for i in range(2, 21):
+        ns.receive(i, node.id, sim.gateways[0].id, low)
+
+    assert node.sf == node.initial_sf - 2
+    assert node.tx_power == node.initial_tx_power
     assert node.snr_history == []
